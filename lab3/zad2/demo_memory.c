173674a47334f2a0b8e4c5279311d1ad6b1f957c
Grzegorz Jasinski <gjasinski@hotmail.com>
2017-03-24 18:32:01 +0100

    zad3

---
+++ lab3/zad2/demo_memory.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[]) {
+    printf("I'll eat your memory \n");
+    char* a = (char*)malloc(1024*1024*10);
+    for(int i = 0; i< 1024*1024*10; i++){
+        a[i]=10;
+    }
+    return 0;
+}
--- lab3/zad2/main.c
+++ lab3/zad2/main.c
@@ -4,6 +4,10 @@
 #include <unistd.h>
 #include <string.h>
 #include <sys/wait.h>
+#include <sys/times.h>
+#include <sys/resource.h>
+#include <bits/time.h>
+#include <math.h>
 
 #define LINE_MAX 100
 #define BUFFER_SIZE 200
@@ -11,11 +15,14 @@
 int read_file_to_buffer(int file_desc, char* buff);
 int read_line(char* buffer, char* line, int position);
 void environment_variable_handler(char* line);
-int thread_handler(char* line);
+int thread_handler(char* line, unsigned int rlimit_cpu, unsigned int rlimit_as);
+int set_limit(int resource, unsigned int value);
+void print_usage();
+
 
 int main(int argc, char* argv[]) {
-    if(argc != 2){
-        printf("%s\n", "main <batch file name>");
+    if(argc != 4){
+        printf("%s\n", "main <batch file name> <max processor time> <max virtual memory in bytes>");
         return 0;
     }
     int batch_file = open(argv[1], O_RDONLY);
@@ -33,7 +40,7 @@ int main(int argc, char* argv[]) {
             line = (char*)calloc(LINE_MAX, sizeof(char));
             position = read_line(buffer, line, position);
             if(line[0] == '#') environment_variable_handler(line + 1);
-            else if(line[0] != 0) thread_handler(line);
+            else if(line[0] != 0) thread_handler(line, (unsigned int)strtol(argv[2], NULL, 10), (unsigned int)strtol(argv[3], NULL, 10) * 1048576);
             free(line);
         }
     }while(buffered_chars == BUFFER_SIZE);
@@ -84,8 +91,8 @@ void environment_variable_handler(char* line){
     }
 }
 
-int thread_handler(char* line){
-    int count = 0;
+int thread_handler(char* line, unsigned int rlimit_cpu, unsigned int rlimit_as){
+    unsigned int count = 0;
     for(int i = 0; i < strlen(line); i++){
         if (line[i] == ' '){
             count++;
@@ -104,21 +111,44 @@ int thread_handler(char* line){
     int return_value;
     int pid = fork();
     if(pid == 0) {
+        if(set_limit(RLIMIT_CPU, rlimit_cpu) != 0 || set_limit(RLIMIT_AS, rlimit_as) != 0){
+            exit(-1);
+        }
         int res = execvp(line, args);
         if(res == -1) abort();
         else exit(res);
     }
     else{
         waitpid(pid, &return_value, 0);
+        print_usage();
         if(WIFSIGNALED(return_value)){
             printf("Abnormal termination, command = %s, signal number = %d\n", line, WTERMSIG(return_value));
             exit(return_value);
         }
-        if(WEXITSTATUS(return_value) == 0) return 0;
-        else {
-            printf("Abnormal exit, command = %s, signal number = %d\n", line, WEXITSTATUS(return_value));
-            exit(WEXITSTATUS(return_value));
+        if(WIFSTOPPED(return_value)){
+            printf("Stopped %d\n, ", WSTOPSIG(return_value));
         }
+        if(WIFEXITED(return_value))
+            if(WEXITSTATUS(return_value) == 0) return 0;
+            else {
+                printf("%d\n", WEXITSTATUS(return_value));
+                printf("Abnormal exit, command = %s, signal number = %d\n", line, WEXITSTATUS(return_value));
+                exit(WEXITSTATUS(return_value));
+            }
 
     }
+}
+
+int set_limit(int resource, unsigned int value){
+    struct rlimit limit;
+    limit.rlim_max = value;
+    limit.rlim_cur = value;
+    return setrlimit(resource, &limit);
+
+}
+
+void print_usage(){
+    struct rusage s_rusage;
+    getrusage(RUSAGE_CHILDREN, &s_rusage);
+    printf("Times: user = %f, system %f\n", s_rusage.ru_utime.tv_sec + s_rusage.ru_utime.tv_usec/pow(10.0, 6.0), s_rusage.ru_stime.tv_sec + s_rusage.ru_stime.tv_usec/pow(10.0, 6.0));
 }
